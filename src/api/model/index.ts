c5c3c98fe2d04634a6af26b26e4fcc7eb445f9eb
Michael D. Norman <michael.norman@denormans.com>
2021-11-25 20:42:46 -0600

    chore: remove graphql-relay

--- src/api/model/index.ts
+++ src/api/model/index.ts
@@ -1,10 +1,6 @@
 import { GraphQLObjectType } from 'graphql';
 
-import { nodeField } from './nodeDefinitions';
-
 export const Query = new GraphQLObjectType({
   name: 'Query',
-  fields: {
-    node: nodeField,
-  },
+  fields: {},
 });
